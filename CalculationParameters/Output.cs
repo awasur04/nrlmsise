6f1fb12c03315326b2d3ede900e1fa988edd29d8
awasur04 <awasur04@yahoo.com>
2022-06-16 16:15:24 +0200

    Added output model
    Added model to represent our output

---
+++ CalculationParameters/Output.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nrlmsise
+{
+    internal class Output
+    {
+        #region Properties
+        double[] densities;
+        double[] temperature;
+        #endregion
+
+        #region Constructor
+        public Output()
+        {
+            this.densities = new double[9];
+            this.temperature = new double[2];
+        }
+        #endregion
+
+        #region Getters & Setters
+        public double[] Densities
+        { 
+            get { return this.densities;}
+            set { this.densities = value; }
+        }
+
+        public double[] Temperature
+        {
+            get { return this.temperature;}
+            set { this.temperature = value; }
+        }
+        #endregion
+    }
+}
