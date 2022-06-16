e29d2db7c8adb507a08e1147e3b732d514001d94
awasur04 <awasur04@yahoo.com>
2022-06-16 16:14:51 +0200

    Added flag model
    Implemented a model to represent our flags for calculations

---
+++ CalculationParameters/Flags.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nrlmsise
+{
+ /*   
+ *   Switches: to turn on and off particular variations use these switches.
+ *   0 is off, 1 is on, and 2 is main effects off but cross terms on.
+ *
+ *   Standard values are 0 for switch 0 and 1 for switches 1 to 23. The 
+ *   array "switches" needs to be set accordingly by the calling program. 
+ *   The arrays sw and swc are set internally.
+ *
+ *   switches[i]:
+ *    i - explanations
+ *   -----------------
+ *    0 - output in meters and kilograms instead of centimeters and grams
+ *    1 - F10.7 effect on mean
+ *    2 - time independent
+ *    3 - symmetrical annual
+ *    4 - symmetrical semiannual
+ *    5 - asymmetrical annual
+ *    6 - asymmetrical semiannual
+ *    7 - diurnal
+ *    8 - semidiurnal
+ *    9 - daily ap [when this is set to -1 (!) the pointer
+ *                  ap_a in struct nrlmsise_input must
+ *                  point to a struct ap_array]
+ *   10 - all UT/long effects
+ *   11 - longitudinal
+ *   12 - UT and mixed UT/long
+ *   13 - mixed AP/UT/LONG
+ *   14 - terdiurnal
+ *   15 - departures from diffusive equilibrium
+ *   16 - all TINF var
+ *   17 - all TLB var
+ *   18 - all TN1 var
+ *   19 - all S var
+ *   20 - all TN2 var
+ *   21 - all NLB var
+ *   22 - all TN3 var
+ *   23 - turbo scale height var
+ */
+    internal class Flags
+    {
+        int[] switches;
+        double[] sw;
+        double[] swc;
+
+        public Flags()
+        {
+            this.switches = new int[24];
+            this.sw = new double[24];
+            this.swc = new double[24];
+        }
+
+        public int[] Switches
+        {
+            get { return switches; }
+            set { switches = value; }
+        }
+
+        public double[] Sw
+        {
+            get { return sw; }
+        }
+
+        public double[] Swc
+        {
+            get { return swc; }
+        }
+
+        public void computeFlags()
+        {
+            for (int i = 0; i < switches.Length; i++)
+            {
+                //Ignore daily AP
+                if (i != 9)
+                {
+                    //Set the sw flags
+                    if (switches[i] == 1)
+                    {
+                        sw[i] = 1;
+                    }
+                    else
+                    {
+                        sw[i] = 0;
+                    }
+
+                    //Set the swc flag
+                    if (switches[i] > 0)
+                    {
+                        swc[i] = 1;
+                    }
+                    else
+                    {
+                        swc[i] = 0;
+                    }
+                }
+                //Handle daily AP flag
+                else
+                {
+                    sw[i] = switches[i];
+                    swc[i] = switches[i];
+                }
+            }
+        }
+
+
+    }
+}
