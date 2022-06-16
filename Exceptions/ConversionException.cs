323e2f727a48740f3996139d3459c997c070946c
awasur04 <awasur04@yahoo.com>
2022-06-16 16:17:14 +0200

    Created custom exception for when invalid data is input for profile options
    Added custom exception which can store the calling textbox to display error to user

---
+++ Exceptions/ConversionException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace nrlmsise.Exceptions
+{
+    internal class ConversionException : Exception
+    {
+        public TextBox callingElement;
+        public ConversionException(TextBox callingElement)
+        {
+;            this.callingElement = callingElement;
+        }
+    }
+}
