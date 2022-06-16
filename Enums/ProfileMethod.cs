b11ee6e952efa945e40d51b1b87253b6c317690d
awasur04 <awasur04@yahoo.com>
2022-06-16 16:16:32 +0200

    Added enum for profile method input
    Added enum to represent profile methods

---
+++ Enums/ProfileMethod.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nrlmsise.Enums
+{
+    public enum ProfileMethod
+    {
+        ALTITUDE,
+        LATITUDE,
+        LONGITUDE,
+        MONTH,
+        DAY_OF_MONTH,
+        DAY_OF_YEAR,
+        HOUR_OF_DAY
+    }
+}
