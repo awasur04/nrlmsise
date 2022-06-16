a23c57a1f134e7dfa172dc3f1e1c1bcaa31fd2ac
awasur04 <awasur04@yahoo.com>
2022-06-16 16:19:05 +0200

    Added model for profile options
    added a model to represent our profile options and store the range

---
+++ Utilities/ProfileOption.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using nrlmsise.Enums;
+
+namespace nrlmsise
+{
+    internal class ProfileOption
+    {
+        #region Properties
+        public ProfileMethod method { get; set; }
+        public double startValue { get; set; }
+        public double stopValue { get; set; }
+        public double stepValue { get; set; }
+        #endregion
+
+        #region Constructor
+
+        public ProfileOption(ProfileMethod method, double startValue, double stopValue, double stepValue)
+        {
+            this.method = method;
+            this.startValue = startValue;
+            this.stopValue = stopValue;
+            this.stepValue = stepValue;
+        }
+
+        #endregion
+
+    }
+}
