0bcc4990892ddf6587ed7b0942ad4e4ba5c5e55f
awasur04 <awasur04@yahoo.com>
2022-06-16 16:13:36 +0200

    Added AP class model
    Added the class model for AP magnetic values

---
+++ CalculationParameters/AP.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nrlmsise
+{
+    internal class AP
+    {
+        double[] ap_array;
+
+        /* Array containing the following magnetic values:
+        *   0 : daily AP
+        *   1 : 3 hr AP index for current time
+        *   2 : 3 hr AP index for 3 hrs before current time
+        *   3 : 3 hr AP index for 6 hrs before current time
+        *   4 : 3 hr AP index for 9 hrs before current time
+        *   5 : Average of eight 3 hr AP indicies from 12 to 33 hrs 
+        *           prior to current time
+        *   6 : Average of eight 3 hr AP indicies from 36 to 57 hrs 
+        *           prior to current time 
+        */
+
+        public AP()
+        {
+            this.ap_array = new double[7];
+        }
+
+        public double[] Ap_Array
+        {
+            get { return this.ap_array; }
+            set { this.ap_array = value; }
+        }
+    }
+}
