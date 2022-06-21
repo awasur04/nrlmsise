aca3e32c1154162c84e8c29a614bbc8f67c2c4c7
awasur04 <awasur04@yahoo.com>
2022-06-21 10:07:34 +0200

    Finished JSONOutput model
    Finished the JSONOutput model for serializing into user friendly format

--- Models/JSONOutput.cs
+++ Models/JSONOutput.cs
@@ -8,46 +8,64 @@ namespace nrlmsise.Models
 {
     internal class JSONOutput
     {
-        Input inputParameters;
-        JSONOutputProfile[] ProfileOutputs;
+        public Input inputParameters;
+        public JSONOutputProfile[] ProfileOutputs;
 
-        public JSONOutput(ProfileOption[] profileOptions, Test[][] testData)
+        public JSONOutput(ProfileOption[] profileOptions, Test[][] testData, Input defaultInputParameters)
         {
+            inputParameters = defaultInputParameters;
+            ProfileOutputs = new JSONOutputProfile[testData.Length];
 
+            for (int i = 0; i < testData.Length; i++)
+            {
+                ProfileOutputs[i] = new JSONOutputProfile(profileOptions[i], testData[i]);
+            }
         }
     }
 
     internal class JSONOutputProfile
     {
-        string ProfileOption;
-        JSONTest Tests;
+        public string ProfileOptions;
+        public Dictionary<string, JSONTestOutput> TestResults;
 
-    }
+        public JSONOutputProfile(ProfileOption profile, Test[] currentTest)
+        {
+            double currentValue = profile.startValue;
 
-    internal class JSONTest
-    {
-        Dictionary<string, JSONTestOutput> TestResults;
+            ProfileOptions = profile.method.ToString() + "(" + profile.startValue + ", " + profile.stopValue + ", " + profile.stepValue + ")";
+            TestResults = new Dictionary<string, JSONTestOutput>();
+
+            for (int i = 0; i < currentTest.Length; i++)
+            {
+                if (currentValue <= profile.stopValue)
+                {
+                    TestResults.Add(profile.method.ToString() + " " + currentValue, new JSONTestOutput(currentTest[i].Output));
+                    currentValue += profile.stepValue;
+                }
+            }
+        }
     }
 
     internal class JSONTestOutput
     {
-        Dictionary<string, string> Results;
+        //Add scientifc display
+        public Dictionary<string, string> Results;
 
         public JSONTestOutput(Output testOutput)
         {
             Results = new Dictionary<string, string>
             {
-                { "TINF", testOutput.Temperature[0].ToString() },
-                { "TG", testOutput.Temperature[1].ToString() },
-                { "HE", testOutput.Densities[0].ToString() },
-                { "O", testOutput.Densities[1].ToString() },
-                { "N2", testOutput.Densities[2].ToString() },
-                { "O2", testOutput.Densities[3].ToString() },
-                { "AR", testOutput.Densities[4].ToString() },
-                { "H", testOutput.Densities[6].ToString() },
-                { "N", testOutput.Densities[7].ToString() },
-                { "ANM", testOutput.Densities[8].ToString() },
-                { "RHO", testOutput.Densities[5].ToString() }
+                { "TINF", ConvertToScientific(testOutput.Temperature[0]) },
+                { "TG", ConvertToScientific(testOutput.Temperature[1]) },
+                { "HE", ConvertToScientific(testOutput.Densities[0]) },
+                { "O", ConvertToScientific(testOutput.Densities[1]) },
+                { "N2", ConvertToScientific(testOutput.Densities[2]) },
+                { "O2", ConvertToScientific(testOutput.Densities[3]) },
+                { "AR", ConvertToScientific(testOutput.Densities[4]) },
+                { "H", ConvertToScientific(testOutput.Densities[6]) },
+                { "N", ConvertToScientific(testOutput.Densities[7]) },
+                { "ANM", ConvertToScientific(testOutput.Densities[8]) },
+                { "RHO", ConvertToScientific(testOutput.Densities[5]) }
             };
         }
     }
